c4dfc4353395b2e3395aee17a8ce2fc63bf7e02d
Alex Vasilescu <alex.a.vasilescu@gmail.com>
2016-05-08 14:25:38 -0700

    Add function DTAccuracy to dt_author_id.py.

--- decision_tree/dt_author_id.py
+++ decision_tree/dt_author_id.py
@@ -20,12 +20,25 @@ from email_preprocess import preprocess
 features_train, features_test, labels_train, labels_test = preprocess()
 
 
+def DTAccuracy(features_train, features_test, labels_train, labels_test):
+    
+    #import sklearn modules for DecisionTree
+    from sklearn import tree
+    from sklearn.metrics import accuracy_score
 
+    #Create classifier using min_sample_split of 40
+    clf = tree.DecisionTreeClassifier(min_samples_split=40);
+    
+    #Fit classifier on the training data
+    clf = clf.fit(features_train, labels_train);
 
-#########################################################
-### your code goes here ###
+    #Use classifier to predict labels for test features
+    predication = clf.predict(features_test);
+    
+    #Calculate accuracy from test with labels_test
+    accuracy = accuracy_score(predication, labels_test);
 
+    return accuracy;
 
-#########################################################
-
-
+accuracy = DTAccuracy(features_train, features_test, labels_train, labels_test);
+print accuracy;
