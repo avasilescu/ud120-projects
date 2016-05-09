0977a8fae09d4d79273e970688a3bba24c8cb068
Alex Vasilescu <alex.a.vasilescu@gmail.com>
2016-05-08 17:06:26 -0700

    Add KNeighborClassifier and AdaboostClassifier. Utilize if statement and variable to switch classifier. Surpress initial graph generation.

--- choose_your_own/your_algorithm.py
+++ choose_your_own/your_algorithm.py
@@ -24,19 +24,88 @@ plt.scatter(grade_slow, bumpy_slow, color = "r", label="slow")
 plt.legend()
 plt.xlabel("bumpiness")
 plt.ylabel("grade")
-plt.show()
+#plt.show()
 ################################################################################
 
 
 ### your code here!  name your classifier object clf if you want the 
 ### visualization code (prettyPicture) to show you the decision boundary
 
+#Imports for Accuracy Score
+import sys
+from time import time
+from sklearn.metrics import accuracy_score
 
+#Imports for KNeighborsClassifier
+from sklearn.neighbors import KNeighborsClassifier
+import numpy
 
+#Imports for Adaboost
+from sklearn.cross_validation import cross_val_score
+from sklearn.datasets import load_iris
+from sklearn.ensemble import AdaBoostClassifier
 
+#Variable to switch between classifiers to run
+#Options:
+# "knnbrs" = K Nearest Neighbors
+# "ada" = Adaboost
+# "rforest" = Random Forest
+selectedClassifier = "knnbrs";
 
+#K Nearest Neighbors Classifier
+def kNearestNeighbors(features_test, features_train,labels_test,labels_train):    
+    #Setup classifier
+    clf = KNeighborsClassifier(algorithm='ball_tree');
+    
+    #Timing fit algorithm
+    t0 = time();
+    
+    #Fitting data
+    clf = clf.fit(features_train,labels_train);
+    
+    print "Training Time: ", round(time() - t0, 3), "s";
+    
+    #Reset timer for prediction;
+    t0 = time();
+    
+    #Predicting using test data
+    nbrs_predict = clf.predict(features_test);
+    
+    print "Prediction Time: ", round(time() - t0, 3), "s";
+    
+    nbrs_acc = accuracy_score(nbrs_predict,labels_test);
+    
+    print "Accuracy: ", nbrs_acc;
+    return clf;
 
+def adaboost(features_test, features_train, labels_test, labels_train):
+    #Setup Classifier
+    clf = AdaBoostClassifier(n_estimators=100)
+    
+    #Timing Fit Algorithm
+    t0 = time();
+    
+    #Fitting data
+    clf = clf.fit(features_train,labels_train);
+    
+    print "Training Time: ", round(time() - t0, 3), "s";
+    
+    #Reset timer for prediction
+    t0=time();
+    
+    #Predicting using test data
+    ada_predict = clf.predict(features_test);
+    print "Prediction Time: ", round(time() - t0, 3), "s";
+    
+    ada_acc = accuracy_score(ada_predict,labels_test);
+    
+    print "Accuracy: ", ada_acc;
+    return clf;
 
+if selectedClassifier == "knnbrs":
+    clf = kNearestNeighbors(features_test, features_train, labels_test, labels_train);
+if selectedClassifier == "ada":
+    clf = adaboost(features_test, features_train, labels_test, labels_train);
 
 try:
     prettyPicture(clf, features_test, labels_test)
