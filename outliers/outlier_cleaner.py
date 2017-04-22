c5e740ceda0da8dd534b1cfaa89189f3021458eb
Alex Vasilescu <alex.a.vasilescu@gmail.com>
2017-04-21 22:18:22 -0700

    Update "outlier_cleaner.py" with calculate error, finding and remove max error, and remove associated predictions and net worth. Create a new list of tuples to return as cleaned_data.

--- outliers/outlier_cleaner.py
+++ outliers/outlier_cleaner.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python
-import numpy as np; 
+import numpy as np;
 
 def outlierCleaner(predictions, ages, net_worths):
     """
@@ -14,21 +14,40 @@ def outlierCleaner(predictions, ages, net_worths):
     cleaned_data = [];
     resid_error = [];
 
-    temp_list = [predictions, net_worths, resid_error];
-
     clean_percent = .1;
     print "Prediction Length: ", len(predictions);
     num_entries_to_remove = int((len(predictions) * clean_percent)); 
 
 
     for entry in range(len(predictions)):
-        #print "Entry: ", entry;
-        #print "Prediction: ", predictions[entry], " Net Worth: " , net_worths[entry];
-        temp_ans = predictions[entry] - net_worths[entry];
+        temp_ans = float(predictions[entry] - net_worths[entry]);
         resid_error.append(temp_ans);
-        #print "Residue Error: " , resid_error[entry];
     
+    for i in range(num_entries_to_remove):
+        max_val = max(resid_error);
+        min_val = min(resid_error);
+        max_idx = resid_error.index(max_val);
+        min_idx = resid_error.index(min_val);
+       
+        if( max_val > abs(min_val)):
+            resid_error.pop(max_idx);
+            temp_predictions = np.delete(predictions, max_idx);
+            temp_net_worths = np.delete(net_worths, max_idx);
+            temp_ages = np.delete(ages, max_idx);
+
+        else:
+            resid_error.pop(min_idx);
+            temp_predictions = np.delete(predictions, min_idx);
+            temp_net_worths = np.delete(net_worths, min_idx);
+            temp_ages = np.delete(ages, min_idx);
+            
+        predictions = temp_predictions;
+        net_worths = temp_net_worths;
+        ages = temp_ages;
+
+    #cleaned_data = (ages, net_worths, resid_error);
+    for i in range(len(ages)):
+        cleaned_data.append((ages[i],net_worths[i],resid_error[i]));
+
+    return cleaned_data;
     
-
-    return cleaned_data
-
